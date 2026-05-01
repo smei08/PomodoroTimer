7f7650dd13a708489819cdeeabddf29aa84deecc
Stephanie Mei <smei826@gmail.com>
2026-05-01 16:26:12 -0400

    added local storage for quotes component

--- src/components/Quotes/Qutoes.jsx
+++ src/components/Quotes/Qutoes.jsx
@@ -3,24 +3,46 @@ import { useState, useEffect } from "react";
 function Quotes() {
   const [quotes, setQuotes] = useState("");
   const [author, setAuthor] = useState("");
-  useEffect(() => {
-    async function getQuoteAPI() {
-      try {
-        const response = await fetch("https://dummyjson.com/quotes/random");
-        if (!response.ok) return;
 
-        const { quote: text, author: fetchAuthor } = await response.json();
-        // const data = await response.json();
-        // console.log(data);
-        setQuotes(text);
-        setAuthor(fetchAuthor);
-      } catch (err) {
-        console.error("Failed to fetch quote:", err);
+  useEffect(() => {
+    const savedData = JSON.parse(localStorage.getItem("savedData"));
+    let quoteInterval;
+
+    if (savedData !== null) {
+      setQuotes(savedData.quotes);
+      setAuthor(savedData.author);
+    } else {
+      async function getQuoteAPI() {
+        try {
+          const response = await fetch("https://dummyjson.com/quotes/random");
+          if (!response.ok) return;
+
+          const { quote: text, author: fetchAuthor } = await response.json();
+
+          setQuotes(text);
+          setAuthor(fetchAuthor);
+        } catch (err) {
+          console.error("Failed to fetch quote:", err);
+        }
       }
+      quoteInterval = setInterval(getQuoteAPI, 1000 * 60 * 60);
+      getQuoteAPI();
     }
-    setInterval(getQuoteAPI, 1000 * 60 * 60);
-    getQuoteAPI();
+    return () => clearInterval(quoteInterval);
   }, []);
+
+  useEffect(() => {
+    if (quotes === "" && author === "") return;
+
+    localStorage.setItem(
+      "savedData",
+      JSON.stringify({
+        quotes,
+        author,
+      }),
+    );
+  }, [quotes, author]);
+
   return (
     <blockquote id="quote-OTD">
       <p className="quote">{quotes}</p>
---
+++ src/components/Timer/Timer.jsx
@@ -0,0 +1,11 @@
+import { useState, useEffect } from "react";
+
+function Timer() {
+
+
+  return (
+
+  );
+}
+
+export default Timer;
